1edca850805f2934b9bab70462fe4d3c123d46a0
Subramanian <mailmemani@ymail.com>
2015-02-05 11:48:08 +0530

    Pod spec updated

---
+++ Classes/PublicHeader.h
@@ -0,0 +1,13 @@
+//
+//  PublicHeader.h
+//  TTPLAPIManager
+//
+//  Created by Subramanian on 2/5/15.
+//  Copyright (c) 2015 Subramanian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "APIBase.h"
+#import "APIKeys.h"
+#import "APIManager.h"
